958d6e01436388390a1d4a2b90960f29047c54a7
zacharia <zakaria.sao@gmail.com>
2019-02-03 16:04:26 +0100

    Update ViewModelModule.kt

--- app/src/main/java/com/example/zach/weatherapp/utils/Modules/ViewModelModule.kt
+++ app/src/main/java/com/example/zach/weatherapp/utils/Modules/ViewModelModule.kt
@@ -1,10 +1,27 @@
 package com.example.zach.weatherapp.utils.Modules
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import com.example.zach.weatherapp.viewModel.CityListViewModel
+import com.example.zach.weatherapp.viewModel.CityListViewModelFactory
+import com.example.zach.weatherapp.viewModel.ForecastViewModel
+import dagger.Binds
 import dagger.Module
 import dagger.Provides
+import dagger.multibindings.IntoMap
 
 @Module
-class ViewModelModule {
+abstract class ViewModelModule {
+    @Binds
+    @IntoMap
+    @ViewModelKey(CityListViewModel::class)
+    abstract fun bindRepoViewModel(repoViewModel: CityListViewModel): ViewModel
 
+    @Binds
+    @IntoMap
+    @ViewModelKey(ForecastViewModel::class)
+    abstract fun bindRepoViewModel(repoViewModel: ForecastViewModel): ViewModel
+
+    @Binds
+    abstract fun bindViewModelFactory(factory: CityListViewModelFactory): ViewModelProvider.Factory
 }
