3cd13a80412223119c266fe6b236ff797d95b777
zacharia <zakaria.sao@gmail.com>
2019-02-11 23:51:31 +0100

    declare SearchableActivity as searchable for MainActivity

--- app/src/main/java/com/example/zach/weatherapp/SearchableActivity.kt
+++ app/src/main/java/com/example/zach/weatherapp/SearchableActivity.kt
@@ -1,12 +1,23 @@
 package com.example.zach.weatherapp
 
+import android.app.SearchManager
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import timber.log.Timber
 
 class SearchableActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_searchable)
+
+        // Verify the action and get the query
+        if (Intent.ACTION_SEARCH == intent.action) {
+            intent.getStringExtra(SearchManager.QUERY)?.also { query ->
+                //doMySearch(query)
+                Timber.d("received action search intent")
+            }
+        }
     }
 }
