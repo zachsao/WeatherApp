180c6a9fb6163f506df5111f29c9a32ba535b60b
zacharia <zakaria.sao@gmail.com>
2019-02-13 10:36:15 +0100

    Update ForecastAdapter.kt

--- app/src/main/java/com/example/zach/weatherapp/Adapter/ForecastAdapter.kt
+++ app/src/main/java/com/example/zach/weatherapp/Adapter/ForecastAdapter.kt
@@ -1,4 +1,33 @@
 package com.example.zach.weatherapp.Adapter
 
-class ForecastAdapter(var mData: List<WeekForecast>) {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.zach.weatherapp.R
+import com.example.zach.weatherapp.data.WeeklyForecast
+import com.example.zach.weatherapp.databinding.ForecastItemBinding
+
+class ForecastAdapter(var mData: List<WeeklyForecast>) : RecyclerView.Adapter<ForecastAdapter.ForecastViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ForecastViewHolder {
+        val inflater =  LayoutInflater.from(parent.context)
+
+        val binding = ForecastItemBinding.inflate(inflater,parent,false)
+
+        return ForecastViewHolder(binding)
+    }
+
+    override fun getItemCount() = mData.size
+
+    override fun onBindViewHolder(holder: ForecastViewHolder, position: Int) {
+        holder.bind(mData[position])
+    }
+
+    inner class ForecastViewHolder(var binding: ForecastItemBinding) : RecyclerView.ViewHolder(binding.root){
+        fun bind(weeklyForecast: WeeklyForecast){
+            binding.dayTextView.text = weeklyForecast.forecastList[0].date
+            binding.forecastIcon.setImageResource(R.drawable.sunny)
+            binding.forecastMaxTempTextview.text = "${weeklyForecast.forecastList[0].main.temp_max}"
+            binding.forecastMinTempTextview.text = "${weeklyForecast.forecastList[0].main.temp_min}"
+        }
+    }
 }
