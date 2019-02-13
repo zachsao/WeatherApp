b2818f4dda6db1952e8efcd89992f32be2e21304
zacharia <zakaria.sao@gmail.com>
2019-02-13 11:33:20 +0100

    Update ForecastViewModel.kt
    add getWeekForecast(cityId)

--- app/src/main/java/com/example/zach/weatherapp/viewModel/ForecastViewModel.kt
+++ app/src/main/java/com/example/zach/weatherapp/viewModel/ForecastViewModel.kt
@@ -6,13 +6,19 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.zach.weatherapp.data.City
 import com.example.zach.weatherapp.data.ForecastRepository
+import com.example.zach.weatherapp.data.WeeklyForecast
+import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.observers.DisposableSingleObserver
+import io.reactivex.schedulers.Schedulers
+import timber.log.Timber
 import javax.inject.Inject
 
 class ForecastViewModel @Inject constructor(private var forecastRepo: ForecastRepository ):ViewModel() {
     private var forecast = MutableLiveData<City>()
     private var disposable : CompositeDisposable? = CompositeDisposable()
 
+    private var weeklyForecast = MutableLiveData<List<WeeklyForecast>>()
     
     fun getDetailedWeatherInfo(cityId: Int): LiveData<City> {
         val cache = forecastRepo.getCache().getCitiesFromCache()
@@ -24,6 +30,20 @@ class ForecastViewModel @Inject constructor(private var forecastRepo: ForecastRe
 
 
         return forecast
+    }
+
+    fun getWeekForecast(cityId: Int): LiveData<List<WeeklyForecast>>{
+        disposable!!.add(forecastRepo.get5DayForecast(cityId).subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribeWith(object: DisposableSingleObserver<List<WeeklyForecast>>(){
+                override fun onSuccess(t: List<WeeklyForecast>) {
+                    weeklyForecast.value = t
+                }
+                override fun onError(e: Throwable) {
+                    Timber.e(e.localizedMessage)
+                }
+            }))
+        return weeklyForecast
     }
 
     override fun onCleared() {
