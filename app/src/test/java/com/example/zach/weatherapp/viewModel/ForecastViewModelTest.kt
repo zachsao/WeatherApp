3f424420ad90262329905da9f04f456311c0788a
zacharia <zakaria.sao@gmail.com>
2019-02-25 17:50:04 +0100

    Create ForecastViewModelTest.kt

---
+++ app/src/test/java/com/example/zach/weatherapp/viewModel/ForecastViewModelTest.kt
@@ -0,0 +1,25 @@
+package com.example.zach.weatherapp.viewModel
+
+import org.junit.Before
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class ForecastViewModelTest {
+
+    @Before
+    fun setUp() {
+    }
+
+    @Test
+    fun getForecast() {
+    }
+
+    @Test
+    fun getDetailedWeatherInfo() {
+    }
+
+    @Test
+    fun getWeekForecast() {
+    }
+}
